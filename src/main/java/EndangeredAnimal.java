fb51c9a3a4425ce52dbef22cbabbff490e485561
timothybaraka <timothybaraka44@gmail.com>
2020-02-12 10:15:05 +0300

    Create a test to instantiate a new endangeredanimal id

--- src/main/java/EndangeredAnimal.java
+++ src/main/java/EndangeredAnimal.java
@@ -1,3 +1,16 @@
 public class EndangeredAnimal {
 
+    private int id;
+    private String name;
+    private String health;
+    private String age;
+
+    public EndangeredAnimal(int id, String name,String health,String age){
+        this.id = id;
+        this.name = name;
+        this.health = health;
+        this.age = age;
+    }
+
+
 }
--- src/test/java/EndangeredAnimalTest.java
+++ src/test/java/EndangeredAnimalTest.java
@@ -9,5 +9,11 @@ public class EndangeredAnimalTest {
         EndangeredAnimal endangeredanimal = new EndangeredAnimal(1, "rhino","healthy","young");
         assertEquals(true, endangeredanimal instanceof EndangeredAnimal );
     }
+    @Test
+    public void InitializeNewEndangeredAnimal_1() throws Exception{
+        EndangeredAnimal endangeredanimal = new EndangeredAnimal(1, "rhino","healthy","young");
+        assertEquals(1, endangeredanimal.getId);
+    }
+
 
 }
